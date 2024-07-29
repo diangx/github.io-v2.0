a631d82e723c6faf16b672fd00172ed1aa02834f
jiwan kim <wldhks1004@naver.com>
2024-07-29 22:39:01 +0800

    add : init vue config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+    lintOnSave:false,
+    transpileDependencies: true,
+    chainWebpack: (config) => {
+        config.plugin('define').tap((args) => {
+            const webpackConfig = args[0];
+            webpackConfig['__VUE_PROD_DEVTOOLS__'] = false;
+            webpackConfig['__VUE_PROD_HYDRATION_WARNING__'] = false;
+            webpackConfig['__VUE_PROD_HYDRATION_MISMATCH__'] = false;
+            webpackConfig['__VUE_PROD_HYDRATION_MISMATCH_DETAILS__'] = false;
+            return args;
+        });
+    }
 })
