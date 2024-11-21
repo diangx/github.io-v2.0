7556dac927d0ed2b29d02e824dd5a29e4115a561
jiwan kim <jiwan.kim@kaonbroadband.com>
2024-11-22 00:22:02 +0900

    ref: config build root & prepare 404 err

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,8 @@ const CUSTOM = require('./env.json')
 process.env.STATE = CUSTOM.STATE
 
 module.exports = defineConfig({
+    outputDir: 'docs', // 빌드 결과물을 docs 디렉토리에 생성
+    publicPath: './',  // 상대 경로로 설정하여 GitHub Pages와 같은 정적 서버에서 문제 방지
     lintOnSave:false,
     transpileDependencies: true,
     chainWebpack: (config) => {
